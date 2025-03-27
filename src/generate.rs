733b2ccdf7811186e3669b916103fafcaf20397e
Michał Pokrywka <wolfmoon@o2.pl>
2025-03-27 16:25:27 +0100

    Added styling (#7)

--- src/generate.rs
+++ src/generate.rs
@@ -4,11 +4,15 @@ use pulldown_cmark::{
     Alignment, BlockQuoteKind, CodeBlockKind, CowStr, Event, Event::*, HeadingLevel, LinkType, Tag,
     TagEnd,
 };
-use std::collections::{HashMap, VecDeque};
-use vertigo::{log, DomElement, DomNode, DomText};
+use std::{
+    collections::{HashMap, VecDeque},
+    rc::Rc,
+};
+use vertigo::{log, Css, DomElement, DomNode, DomText};
 
 #[cfg(feature = "syntect")]
 use crate::highlighting::highlight;
+use crate::styling::CMarkStyle;
 
 enum TableState {
     Head,
@@ -30,6 +34,8 @@ struct VertigoWriter<'a, I> {
     // Stack of nested nodes
     soc: VecDeque<DomNode>,
 
+    styling: Rc<CMarkStyle>,
+
     #[cfg(feature = "syntect")]
     in_code_block: Option<CowStr<'a>>,
 }
@@ -38,7 +44,7 @@ impl<'a, I> VertigoWriter<'a, I>
 where
     I: Iterator<Item = Event<'a>>,
 {
-    fn new(iter: I) -> Self {
+    fn new(iter: I, styling: CMarkStyle) -> Self {
         Self {
             iter,
             in_non_writing_block: false,
@@ -47,13 +53,14 @@ where
             table_cell_index: 0,
             numbers: HashMap::new(),
             soc: VecDeque::new(),
+            styling: Rc::new(styling),
             #[cfg(feature = "syntect")]
             in_code_block: None,
         }
     }
 
     fn run(mut self) -> DomNode {
-        self.push_elname("div");
+        self.push_element_styled(DomElement::new("div"), &self.styling.clone().container);
         while let Some(event) = self.iter.next() {
             match event {
                 Start(tag) => {
@@ -110,11 +117,13 @@ where
                     let link = DomElement::new("a").attr("href", ["#", name.as_ref()].concat());
                     let number = *self.numbers.entry(name).or_insert(len);
                     link.add_child_text(number.to_string());
-                    self.add_child(
-                        DomElement::new("sup")
-                            .attr("class", "footnote-reference")
-                            .child(link),
-                    );
+                    let mut element = DomElement::new("sup")
+                        .attr("class", "footnote-reference")
+                        .child(link);
+                    if !self.styling.sup.groups.is_empty() {
+                        element = element.css(&self.styling.sup);
+                    }
+                    self.add_child(element);
                 }
                 TaskListMarker(true) => {
                     self.add_child(
@@ -141,10 +150,11 @@ where
 
     /// Pushes dom element on stack
     fn start_tag(&mut self, tag: Tag<'a>) {
+        let styling = self.styling.clone();
         match &tag {
             Tag::HtmlBlock => {}
             Tag::Paragraph => {
-                self.push_elname("p");
+                self.push_elname("p", &styling.p);
             }
             Tag::Heading {
                 level,
@@ -152,13 +162,13 @@ where
                 classes,
                 attrs: _, // Vertigo doesn't support dynamic attributes keys
             } => {
-                let el_name = match level {
-                    HeadingLevel::H1 => "h1",
-                    HeadingLevel::H2 => "h2",
-                    HeadingLevel::H3 => "h3",
-                    HeadingLevel::H4 => "h4",
-                    HeadingLevel::H5 => "h5",
-                    HeadingLevel::H6 => "h6",
+                let (el_name, css) = match level {
+                    HeadingLevel::H1 => ("h1", &styling.h1),
+                    HeadingLevel::H2 => ("h2", &styling.h2),
+                    HeadingLevel::H3 => ("h3", &styling.h3),
+                    HeadingLevel::H4 => ("h4", &styling.h4),
+                    HeadingLevel::H5 => ("h5", &styling.h5),
+                    HeadingLevel::H6 => ("h6", &styling.h6),
                 };
                 let element = DomElement::new(el_name);
                 if let Some(id) = id {
@@ -168,26 +178,26 @@ where
                     let value = classes.join(" ");
                     element.add_attr("class", value);
                 }
-                self.push_node(element);
+                self.push_element_styled(element, css);
             }
             Tag::Table(alignments) => {
                 self.table_alignments = alignments.clone();
-                self.push_node(DomElement::new("table").attr("border", "1"))
+                self.push_element_styled(DomElement::new("table"), &styling.table)
             }
             Tag::TableHead => {
                 self.table_state = TableState::Head;
                 self.table_cell_index = 0;
-                self.push_elname("thead");
-                self.push_elname("tr");
+                self.push_elname("thead", &styling.thead);
+                self.push_elname("tr", &styling.tr);
             }
             Tag::TableRow => {
                 self.table_cell_index = 0;
-                self.push_elname("tr");
+                self.push_elname("tr", &styling.tr);
             }
             Tag::TableCell => {
-                let el_name = match self.table_state {
-                    TableState::Head => "th",
-                    TableState::Body => "td",
+                let (el_name, style) = match self.table_state {
+                    TableState::Head => ("th", &styling.th),
+                    TableState::Body => ("td", &styling.td),
                 };
                 let element = DomElement::new(el_name);
                 match self.table_alignments.get(self.table_cell_index) {
@@ -196,22 +206,19 @@ where
                     Some(&Alignment::Right) => element.add_attr("style", "text-align: right"),
                     _ => (),
                 }
-                self.push_node(element);
+                self.push_element_styled(element, style);
             }
             #[cfg(feature = "syntect")]
             Tag::CodeBlock(info) => {
                 if let CodeBlockKind::Fenced(info) = info {
-                    self.push_node(
-                        DomElement::new("pre")
-                            .attr("style", "background-color: black")
-                    );
+                    self.push_element_styled(DomElement::new("pre"), &styling.codeblock);
                     // TODO: info
                     self.in_code_block = Some(info.clone());
                 }
             }
             #[cfg(not(feature = "syntect"))]
             Tag::CodeBlock(info) => {
-                self.push_elname("pre");
+                self.push_element_styled(DomElement::new("pre"), &styling.codeblock);
                 let element = DomElement::new("code");
                 match info {
                     CodeBlockKind::Fenced(info) => {
@@ -237,22 +244,22 @@ where
                     };
                     element.add_attr("class", kind_value);
                 };
-                self.push_node(element);
+                self.push_element_styled(element, &styling.blockquote);
             }
-            Tag::List(Some(1)) => self.push_elname("ol"),
+            Tag::List(Some(1)) => self.push_elname("ol", &styling.ol),
             Tag::List(Some(start)) => {
-                self.push_node(DomElement::new("ol").attr("start", start));
+                self.push_element_styled(DomElement::new("ol").attr("start", start), &styling.ol);
             }
-            Tag::List(None) => self.push_elname("ul"),
-            Tag::Item => self.push_elname("li"),
-            Tag::DefinitionList => self.push_elname("dl"),
-            Tag::DefinitionListTitle => self.push_elname("dt"),
-            Tag::DefinitionListDefinition => self.push_elname("dd"),
-            Tag::Subscript => self.push_elname("sub"),
-            Tag::Superscript => self.push_elname("sup"),
-            Tag::Emphasis => self.push_elname("em"),
-            Tag::Strong => self.push_elname("strong"),
-            Tag::Strikethrough => self.push_elname("del"),
+            Tag::List(None) => self.push_elname("ul", &styling.ul),
+            Tag::Item => self.push_elname("li", &styling.li),
+            Tag::DefinitionList => self.push_elname("dl", &styling.dl),
+            Tag::DefinitionListTitle => self.push_elname("dt", &styling.dt),
+            Tag::DefinitionListDefinition => self.push_elname("dd", &styling.dd),
+            Tag::Subscript => self.push_elname("sub", &styling.sub),
+            Tag::Superscript => self.push_elname("sup", &styling.sup),
+            Tag::Emphasis => self.push_elname("em", &styling.em),
+            Tag::Strong => self.push_elname("strong", &styling.strong),
+            Tag::Strikethrough => self.push_elname("del", &styling.del),
             Tag::Link {
                 link_type,
                 dest_url,
@@ -267,7 +274,7 @@ where
                 if !title.is_empty() {
                     element.add_attr("title", title);
                 }
-                self.push_node(element);
+                self.push_element_styled(element, &styling.a);
             }
             Tag::Image {
                 link_type: _,
@@ -275,9 +282,13 @@ where
                 title,
                 id: _,
             } => {
-                let element = DomElement::new("img")
+                let mut element = DomElement::new("img")
                     .attr("src", dest_url)
                     .attr("alt", self.raw_text());
+
+                if !styling.img.groups.is_empty() {
+                    element = element.css(&styling.img);
+                }
                 if !title.is_empty() {
                     element.add_attr("title", title);
                 }
@@ -286,15 +297,17 @@ where
             Tag::FootnoteDefinition(name) => {
                 let len = self.numbers.len() + 1;
                 let number = *self.numbers.entry(name.clone()).or_insert(len);
+                let mut sup_element = DomElement::new("sup")
+                    .attr("class", "footnote-definition-label")
+                    .child(DomText::new(number.to_string()));
+                if !styling.sup.groups.is_empty() {
+                    sup_element = sup_element.css(&styling.sub);
+                }
                 self.push_node(
                     DomElement::new("div")
                         .attr("class", "footnote-definition")
                         .attr("id", name)
-                        .child(
-                            DomElement::new("sup")
-                                .attr("class", "footnote-definition-label")
-                                .child(DomText::new(number.to_string())),
-                        ),
+                        .child(sup_element),
                 );
             }
             Tag::MetadataBlock(_) => {
@@ -315,7 +328,7 @@ where
                 // </tr></thead><tbody>
                 self.pop_node();
                 self.pop_node();
-                self.push_elname("tbody");
+                self.push_elname("tbody", &self.styling.clone().tbody);
                 self.table_state = TableState::Body;
             }
             TagEnd::TableCell => {
@@ -409,9 +422,21 @@ where
         self.soc.push_front(node.into());
     }
 
-    fn push_elname(&mut self, name: impl Into<String>) {
+    fn push_element_styled(&mut self, element: DomElement, css: &Css) {
+        let mut element = element;
+        if !css.groups.is_empty() {
+            element = element.css(css)
+        }
+        self.soc.push_front(element.into());
+    }
+
+    fn push_elname(&mut self, name: impl Into<String>, css: &Css) {
         let name = name.into();
-        self.push_node(DomElement::new(name));
+        let mut element = DomElement::new(name);
+        if !css.groups.is_empty() {
+            element = element.css(css)
+        }
+        self.push_node(element);
     }
 
     fn pop_node(&mut self) -> Option<DomNode> {
@@ -450,9 +475,9 @@ where
 
 /// Iterate over an iterator of pulldown's events, generate DomNode for each `Event`,
 /// structure it into DOM tree and return the root node.
-pub fn generate_tree<'a, I>(iter: I) -> DomNode
+pub fn generate_tree<'a, I>(iter: I, styling: CMarkStyle) -> DomNode
 where
     I: Iterator<Item = Event<'a>>,
 {
-    VertigoWriter::new(iter).run()
+    VertigoWriter::new(iter, styling).run()
 }
